60af018f649a5f9af1c55a40e68f9a2b74b923be
Swakshan <tsswakshan@gmail.com>
2024-09-11 23:36:30 +0530

    v10.161 : workflow update

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from pprint import pprint
 from common import DUMMY_FOLDER,MAIN_FOLDER,ZIP_FILE,EXTRACT_FOLDER,PKG_NAME,APP_NAME,new_file_name,old_file_name,DEBUG,manifest_file_name,Platform,Releases,new_file_ipad_name,old_file_ipad_name
 from common import writeJson,readJson,get_exception,vercodeGenerator,headers
 
-VER = "v10.16 : minor vercode update"
+VER = "v10.161 : workflow update"
 
 
 def downloader(url,fileName="",isJson=False):
