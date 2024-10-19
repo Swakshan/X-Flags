1f63b91e9fed4abe425247ca5960965dad00c7eb
Swakshan <tsswakshan@gmail.com>
2024-10-19 15:56:42 +0530

    v11.1 : updated headers

--- src/common.py
+++ src/common.py
@@ -81,7 +81,22 @@ def vercodeGenerator(v):
     return vercode
 
 def headers():
-    return {'User-Agent': generate_user_agent()}
+    return {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-GB,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": generate_user_agent(),
+}
 
 def commitLinkFormat(flag_data):
     def countFormat(count,ns="Flags"):
