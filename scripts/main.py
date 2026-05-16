635a459696456155f3d1b3d522282c1cda7e40f4
Swakshan <tsswakshan@gmail.com>
2026-05-16 12:09:38 +0530

    v23.32 : include env in action

--- scripts/main.py
+++ scripts/main.py
@@ -8,7 +8,7 @@ from basics import printCmd,printSubCmd
 from compare import compareFlags
 import argparse
 
-VER = "v23.31 : set default xchat flags"
+VER = "v23.32 : include env in action"
 
 def flagName(data:DATA):
     os.makedirs(MAIN_FOLDER,exist_ok=True)
